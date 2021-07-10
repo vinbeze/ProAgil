bf64046ee513234ff1cd93afcc226d9d230e81b5
Vinicius B <vinicius_bezerra@hotmail.com>
2021-07-10 18:17:43 -0300

    adicionado data annotations

--- ProAgil.API/Dtos/EventoDto.cs
+++ ProAgil.API/Dtos/EventoDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProAgil.API.Dtos
 {
@@ -7,12 +8,17 @@ namespace ProAgil.API.Dtos
     {
         
         public int EventoId { get; set; }
-        public string Local { get; set; }   
+        [Required(ErrorMessage ="O local deve ser Preenchido")]
+        [StringLength(100,MinimumLength =3,ErrorMessage = "Local é entre 3 a 100 caracteres")]        public string Local { get; set; }   
         public string DataEvento { get; set; }
+        [Required(ErrorMessage ="O tema deve ser Preenchido")]
         public string Tema { get; set; }
+        [Range(2,120000, ErrorMessage = "Quantidade de Pessoas é entre 2 e 120000")]
         public int QtdPessoas { get; set; }
         public string ImagemURL { get; set; }
+        [Phone]
         public string Telefone { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
         public List<LoteDto> Lotes { get; set; }
         public List<RedeSocialDto> RedeSociais { get; set; }
--- ProAgil.API/Dtos/RedeSocialDto.cs
+++ ProAgil.API/Dtos/RedeSocialDto.cs
@@ -1,10 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProAgil.API.Dtos
 {
     public class RedeSocialDto
     {
 
         public int id { get; set; }
+        [Required]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo {0} é Obrigatório")]
         public string URL { get; set; }
 
     }
